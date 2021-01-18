9eba3a97d8a40164e9b1b22c21f7184de5c4c134
Rubens Rezende <rurezende@gmail.com>
2021-01-18 06:03:37 -0300

    Inserção dos modulos padrão de M0 a M4

--- include/mega2560.h
+++ include/mega2560.h
@@ -16,5 +16,10 @@
 #ifndef __MEGA2560_H__
 #define __MEGA2560_H__
 
+//Os valores a seguir referen-se ao numero de portas utilizados nos testes, não possuem ligação com o numero de portas do mega 2560
+#define TOTAL_DIO_PORTS             14
+#define TOTAL_ANALOG_INPUT_PORTS    6
+
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -59,13 +59,20 @@
  * and limitations under the License.
  */
 #include <Arduino.h>
+#include <limits.h>
+#include <avr/power.h>
+#include "mega2560.h"
 #include "benchmark.h"
 
 
-/*teste*/
-
 
 const uint8_t benchmark_mode = BENCHMARK_MODE_0;
+int64_t var_add, var_sub;
+double var_mult, var_div;
+float var_analog;
+
+
+
 
 void setup() {
   switch (benchmark_mode) {
@@ -73,7 +80,118 @@ void setup() {
       pinMode(LED_BUILTIN, OUTPUT);
       digitalWrite(LED_BUILTIN, HIGH);
       break;
+
+    case BENCHMARK_MODE_1:
+      for (uint8_t i = 0; i < TOTAL_DIO_PORTS; i++) {
+        pinMode(i, OUTPUT);
+      }
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.begin(9600);
+      break;
+    
+    case BENCHMARK_MODE_2:
+      for (uint8_t i = 3; i < TOTAL_DIO_PORTS; i++) {
+        pinMode(i, INPUT_PULLUP);
+      }
+      pinMode(LED_BUILTIN, OUTPUT);
+      digitalWrite(LED_BUILTIN, LOW);
+
+      pinMode(A0, OUTPUT);
+      pinMode(A1, OUTPUT);
+      pinMode(A2, OUTPUT);
+      pinMode(A3, OUTPUT);
+      pinMode(A4, OUTPUT);
+      pinMode(A5, OUTPUT);
+      pinMode(A6, OUTPUT);
+      pinMode(A7, OUTPUT);      
+      digitalWrite(A0, LOW);
+      digitalWrite(A1, LOW);
+      digitalWrite(A2, LOW);
+      digitalWrite(A3, LOW);
+      digitalWrite(A4, LOW);
+      digitalWrite(A5, LOW);
+      digitalWrite(A6, LOW);
+      digitalWrite(A7, LOW);
+      
+
+      power_adc_disable();
+      power_spi_disable();
+
+      Serial.begin(9600);
+      break;
   
+    case BENCHMARK_MODE_3:
+      for (uint8_t i = 3; i < TOTAL_DIO_PORTS; i++) {
+        pinMode(i, INPUT_PULLUP);
+      }
+      pinMode(LED_BUILTIN, OUTPUT);
+      digitalWrite(LED_BUILTIN, LOW);
+
+      pinMode(A0, OUTPUT);
+      pinMode(A1, OUTPUT);
+      pinMode(A2, OUTPUT);
+      pinMode(A3, OUTPUT);
+      pinMode(A4, OUTPUT);
+      pinMode(A5, OUTPUT);
+      pinMode(A6, OUTPUT);
+      pinMode(A7, OUTPUT);      
+      digitalWrite(A0, LOW);
+      digitalWrite(A1, LOW);
+      digitalWrite(A2, LOW);
+      digitalWrite(A3, LOW);
+      digitalWrite(A4, LOW);
+      digitalWrite(A5, LOW);
+      digitalWrite(A6, LOW);
+      digitalWrite(A7, LOW);
+
+      power_adc_disable();
+      power_spi_disable();
+
+      // Put CPU clock at 8MHz
+      noInterrupts();
+      CLKPR = _BV(CLKPCE);  // enable change of the clock prescaler
+      CLKPR = _BV(CLKPS0);  // divide frequency by 2 = 8 MHz
+      interrupts();
+
+      Serial.begin(115200);
+      break;
+
+    case BENCHMARK_MODE_4:
+      for (uint8_t i = 3; i < TOTAL_DIO_PORTS; i++) {
+        pinMode(i, INPUT_PULLUP);
+      }
+      pinMode(LED_BUILTIN, OUTPUT);
+      digitalWrite(LED_BUILTIN, LOW);
+
+      pinMode(A0, OUTPUT);
+      pinMode(A1, OUTPUT);
+      pinMode(A2, OUTPUT);
+      pinMode(A3, OUTPUT);
+      pinMode(A4, OUTPUT);
+      pinMode(A5, OUTPUT);
+      pinMode(A6, OUTPUT);
+      pinMode(A7, OUTPUT);      
+      digitalWrite(A0, LOW);
+      digitalWrite(A1, LOW);
+      digitalWrite(A2, LOW);
+      digitalWrite(A3, LOW);
+      digitalWrite(A4, LOW);
+      digitalWrite(A5, LOW);
+      digitalWrite(A6, LOW);
+      digitalWrite(A7, LOW);
+
+      power_adc_disable();
+      power_spi_disable();
+
+      // Put CPU clock at 4MHz
+      noInterrupts();
+      CLKPR = _BV(CLKPCE);  // enable change of the clock prescaler
+      CLKPR = _BV(CLKPS1);  // divide frequency by 4 = 4 MHz
+      interrupts();
+
+      Serial.begin(9600);
+      break;
+
     default:
       break;
   }
@@ -81,10 +199,118 @@ void setup() {
 
 void loop() {
   switch (benchmark_mode) {
-    case BENCHMARK_MODE_0:
-      while (true) {}
+    case BENCHMARK_MODE_0:      
+      break;
+
+    case BENCHMARK_MODE_1:
+      var_add = INT64_MIN;
+      var_sub = INT64_MAX;
+      var_mult = 1;
+      var_div = __DBL_MAX__;
+      var_analog = 0.0f;
+      
+      Serial.println(F("Run"));
+
+      for (uint64_t i = 0; i < 1000; i++) {
+        var_add += 3;
+        var_sub -= 3;
+        var_mult = var_mult * 1.6785;
+        var_div = var_div / 1.6785;        
+      }
+
+      for (uint8_t i = 0; i < TOTAL_DIO_PORTS; i++) {        
+        if (digitalRead(i) == HIGH) {
+          digitalWrite(i, LOW);
+        } else {
+          digitalWrite(i, HIGH);
+        }
+      }
+      
+      var_analog = analogRead(A0);
+      var_analog = analogRead(A1);
+      var_analog = analogRead(A2);
+      var_analog = analogRead(A3);
+      var_analog = analogRead(A4);
+      var_analog = analogRead(A5);
+      var_analog = analogRead(A6);
+      var_analog = analogRead(A7);
       break;
   
+    case BENCHMARK_MODE_2:
+      var_add = INT64_MIN;
+      var_sub = INT64_MAX;
+      var_mult = 1;
+      var_div = __DBL_MAX__;
+      var_analog = 0.0f;
+      
+      Serial.println(F("Run"));
+
+      for (uint64_t i = 0; i < 1000; i++) {
+        var_add += 3;
+        var_sub -= 3;
+        var_mult = var_mult * 1.6785;
+        var_div = var_div / 1.6785;        
+      }
+
+      for (uint8_t i = 0; i < 3; i++) {        
+        if (digitalRead(i) == HIGH) {
+          digitalWrite(i, LOW);
+        } else {
+          digitalWrite(i, HIGH);
+        }
+      }
+      break;
+
+    case BENCHMARK_MODE_3:
+      var_add = INT64_MIN;
+      var_sub = INT64_MAX;
+      var_mult = 1;
+      var_div = __DBL_MAX__;
+      var_analog = 0.0f;
+      
+      Serial.println(F("Run"));
+
+      for (uint64_t i = 0; i < 1000; i++) {
+        var_add += 3;
+        var_sub -= 3;
+        var_mult = var_mult * 1.6785;
+        var_div = var_div / 1.6785;        
+      }
+
+      for (uint8_t i = 0; i < 3; i++) {        
+        if (digitalRead(i) == HIGH) {
+          digitalWrite(i, LOW);
+        } else {
+          digitalWrite(i, HIGH);
+        }
+      }
+      break;
+
+    case BENCHMARK_MODE_4:
+      var_add = INT64_MIN;
+      var_sub = INT64_MAX;
+      var_mult = 1;
+      var_div = __DBL_MAX__;
+      var_analog = 0.0f;
+      
+      Serial.println(F("Run"));
+
+      for (uint64_t i = 0; i < 1000; i++) {
+        var_add += 3;
+        var_sub -= 3;
+        var_mult = var_mult * 1.6785;
+        var_div = var_div / 1.6785;        
+      }
+
+      for (uint8_t i = 0; i < 3; i++) {        
+        if (digitalRead(i) == HIGH) {
+          digitalWrite(i, LOW);
+        } else {
+          digitalWrite(i, HIGH);
+        }
+      }
+      break;
+
     default:
       break;
   }
